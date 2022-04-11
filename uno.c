cc1849b24cc38008005aa14eb8fa60611923135e
felirodriguez <102253954+felirodriguez@users.noreply.github.com>
2022-04-10 23:10:13 -0300

    Update uno.c

--- uno.c
+++ uno.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  double theta = atof (arg[1]);
+  double theta = atof (argv [1]);
   printf("%f\n", cos(theta) + sin(theta) * sin(theta));
   return 0;
 }
